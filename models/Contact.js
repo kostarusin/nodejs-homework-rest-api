832e23467ae8cec591b9eea14698b58830a78e3f
Kostyantyn Tarusin <122443715+kostarusin@users.noreply.github.com>
2023-10-21 14:12:52 +0200

    fix from 3 HW

--- models/Contact.js
+++ models/Contact.js
@@ -1,8 +1,5 @@
 import { Schema, model } from "mongoose";
 import { handleSaveError, runValidatorsAtUpdate } from "./hooks.js";
-import Joi from "joi";
-
-const contactlist = ["private", "corporate"];
 
 const contactSchema = new Schema(
   {
@@ -25,7 +22,7 @@ const contactSchema = new Schema(
     },
     listType: {
       type: String,
-      enum: contactlist,
+      enum: ["private", "corporate"],
       required: true,
     },
   },
@@ -40,28 +37,6 @@ contactSchema.post("save", handleSaveError);
 contactSchema.pre("findOneAndUpdate", runValidatorsAtUpdate);
 
 contactSchema.post("findOneAndUpdate", handleSaveError);
-
-export const contactAddSchema = Joi.object({
-  name: Joi.string().required().messages({
-    "any.required": `"name" required field`,
-  }),
-  email: Joi.string().required().messages({
-    "any.required": `"email" required field`,
-  }),
-  phone: Joi.string().required().messages({
-    "any.required": `"phone" required field`,
-  }),
-  favorite: Joi.boolean(),
-  listType: Joi.string()
-    .valid(...contactlist)
-    .required(),
-});
-
-export const contactUpdateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required().messages({
-    "any.required": `missing field favorite`,
-  }),
-});
 
 const Contact = model("contact", contactSchema);
 
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -4,8 +4,8 @@ import { validateBody } from "../../decorators/index.js";
 import {
   contactAddSchema,
   contactUpdateFavoriteSchema,
-} from "../../models/Contact.js";
-import {isValidId} from "../../middlewares/index.js";
+} from "../../utils/validation/contactValidationSchemas.js";
+import { isValidId } from "../../middlewares/index.js";
 
 const contactAddValidate = validateBody(contactAddSchema);
 const contactUpdateFavoriteValidate = validateBody(contactUpdateFavoriteSchema);
@@ -19,8 +19,8 @@ router.get("/:id", isValidId, contactsController.getById);
 router.post("/", contactAddValidate, contactsController.add);
 
 router.put(
-  "/:id", 
-  isValidId, 
+  "/:id",
+  isValidId,
   contactAddValidate,
   contactsController.updateById
 );
